018ff5524c51017f44628b976be011655da5688e
omarbekhit211 <108030120+omarbekhit211@users.noreply.github.com>
2026-05-05 23:31:57 +0300

    Refactor SystemManagement and room reservation logic
    Added new instances of SystemManagement and modified room reservation logic to accommodate multiple managers and receptionists.

--- test.java
+++ test.java
@@ -4,6 +4,7 @@
  */
 package project2;
  import java.time.LocalDate;
+import java.time.Month;
 import java.time.Period;
 import java.time.format.DateTimeFormatter;
 /**
@@ -29,22 +30,45 @@ public class test {
         
         System.out.println("Enter the admin");
         SystemManagement d1 = new SystemManagement();
+        SystemManagement d2 = new SystemManagement();
         Manager j = new Manager("Ahmed dfd", "ahfe@", "01211545", "0#fFDSG$dsf",d1);
-         Receptionist f= new Receptionist("hhfghff", "omfkd@", "24353546", "fgdfhD#y2");
-         Customer r = new Customer("ahmed mahkouf", "df@dgfg", "0124584848", "dsFdh#55");
+        Manager jj = new Manager("Ahmed dfd", "ahfe@", "01211545", "0#fFDSG$dsf",d2);
+         Receptionist f= new Receptionist("hhfghff", "omfkd@", "24353546", "fgdfhD#y2",d1);
+         Receptionist ff= new Receptionist("hhfghff", "omfkd@", "24353546", "fgdfhD#y2",d2);
+         Customer r = new Customer("ahmed mahkouf", "df@dgfg", "0124584848", "dsFdh#55",d1);
+         Customer rr = new Customer("ahmed mahkouf", "df@dgfg", "0124584848", "dsFdh#55",d2);
         
        
 //         Room df= new Room(60, "available", 12, LocalDate.of(12, 01, 2026));
 //         Room g =new Room(60, "available", 5, LocalDate.of(2026, 01, 9));//why it show error when write 09
         
-         StandardRoom g =new StandardRoom("single", true,60,"available", 5);
-         StandardRoom gfg =new StandardRoom("single", true,60,"available", 5);
-         StandardRoom gg =new StandardRoom("single", true,60,"available", 5);
-         Reservation d = new Reservation(r, g,12.5,"cash", "booked", LocalDate.of(2026, 1, 9), Period.of(1, 5, 0), f);
-         System.out.println(d.toString());
+         StandardRoom g =new StandardRoom("single", true,60,"available", 5,d1);
+         
+         StandardRoom gfg =new StandardRoom("single", true,60,"available", 5,d1);
+         StandardRoom gsfg =new StandardRoom("double", true,60,"available", 5,d2);
+         StandardRoom gsfdg =new StandardRoom("double", true,60,"available", 5,d2);
+         
+         StandardRoom gg =new StandardRoom("single", true,60,"booked", 5,d1);
+//         Reservation d = new Reservation(r, g,12.5,"cash", "booked", LocalDate.of(2026, 1, 9), Period.of(1, 5, 0), f);
+         f.makeReservation(r, gg, 625, "cash", "booked", LocalDate.of(2026, Month.MARCH, 1), Period.ofDays(10));
+         f.makeReservation(r, g, 625, "cash", "booked", LocalDate.of(2026, Month.MARCH, 1), Period.ofDays(10));
+         ff.makeReservation(r, gsfdg, 625, "cash", "booked", LocalDate.of(2026, Month.MARCH, 1), Period.ofDays(10));
+//         System.out.println(d.toString());
+         
          System.out.println(r.number_of_users);
          System.out.println(g.toString());
          System.out.println(gg.getRoom_number());
+         
+         System.out.println(d1.getAllRooms_String());
+         System.out.println(d2.getAllRooms_String());
+        
+//         System.out.println(d1.getAllReservation_String());
+         System.out.println(f.findRoom(5).toString());
+         System.out.println(j.getAllReservation_String());
+         System.out.println(jj.getAllReservation_String());
+         System.out.println(j.getAllUsers_String());
+         System.out.println(jj.getAllUsers_String());
+//         d1.geAllReservation_Array();
          
          
          
